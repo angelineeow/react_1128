ef10ea13766cb1163a6f98e102b2a0ddc790e319
ae1204 <angelineeooww@gmail.com>
2022-12-21 00:57:20 -0500

    hw 12/20

---
+++ JQuery_assignment/ajax.js
@@ -0,0 +1,68 @@
+// definition of boilerplate, bundler, and transpiler
+// Boilerplate is a part of code that is reused multiple times with no to a little modification. Since there is many great quality snippets of codes created today, boilerplate is a good way to start a similar project which requires us to just modify the existing codes.
+// Bundler is a tool that combines multiple JavaScript code files into a single JavaScript code file that can be exceuted in browser.
+// Transpiler is a translator that translates a programming language to another programming language. For instance, we can use Babel to translate ECMAScript 2015+ code to older version that is compatible to browsers and environments.
+  
+// Basic: Get an object with properties including url, data, and success. 
+$.myAjax = function (settings){
+    let xhr = new XMLHttpRequest();
+    xhr.open(settings.method, settings.url);
+    xhr.responseType = settings.dataType;
+    xhr.onload = () => {
+        settings.success(xhr.response);
+    } 
+    xhr.onerror = () => {
+        settings.error(xhr.response);
+    }
+    xhr.send()
+}
+
+// success case
+$.ajax({
+    url: "https://jsonplaceholder.typicode.com/posts",
+    method: "GET",
+    dataType: "json",
+    success: function( result ) {
+      console.log(result)
+    },
+    error: () => {
+        console.error("ERROR for $.ajax")
+    }
+});
+
+$.myAjax({
+    url: "https://jsonplaceholder.typicode.com/posts",
+    method: "GET",
+    dataType: "json",
+    success: function( result ) {
+        console.log(result)
+    },
+    error: () => {
+        console.error("ERROR for $.myAjax")
+    }
+});
+
+//error case
+$.ajax({
+    url: "https://errorjson.com",
+    method: "GET",
+    dataType: "json",
+    success: function(result) {
+        console.log(result)
+    },
+    error: () => {
+        console.error("ERROR for $.ajax")
+    }
+});
+
+$.myAjax({
+    url: "https://errorjson.com",
+    method: "GET",
+    dataType: "json",
+    success: function(result) {
+        console.log(result)
+    },
+    error: () => {
+        console.error("ERROR for $.myAjax")
+    }
+});
