7a647c2c5177b4ca96e7206562eecfe3cb02f5e2
ae1204 <angelineeooww@gmail.com>
2022-12-21 00:59:46 -0500

    edited

--- JQuery_assignment/ajax.js
+++ JQuery_assignment/ajax.js
@@ -1,7 +1,7 @@
 // definition of boilerplate, bundler, and transpiler
-// Boilerplate is a part of code that is reused multiple times with no to a little modification. Since there is many great quality snippets of codes created today, boilerplate is a good way to start a similar project which requires us to just modify the existing codes.
-// Bundler is a tool that combines multiple JavaScript code files into a single JavaScript code file that can be exceuted in browser.
-// Transpiler is a translator that translates a programming language to another programming language. For instance, we can use Babel to translate ECMAScript 2015+ code to older version that is compatible to browsers and environments.
+// Boilerplate is a part of code that is reused multiple times with no to a little modification. Since there is many great quality snippets of codes created today, boilerplate code is useful to create a similar project which requires us to just modify the existing codes.
+// Bundler is a tool that combines multiple JavaScript code files into a single JavaScript code file that can be executed in browser.
+// Transpiler is a translator that translates a programming language to another programming language. For instance, we can use Babel which is a transpiler to translate ECMAScript 2015+ code to older version that is compatible to browsers and environments.
   
 // Basic: Get an object with properties including url, data, and success. 
 $.myAjax = function (settings){
